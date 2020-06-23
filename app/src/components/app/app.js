9c1e323dc5568caec7bb8ed7ef053adc32ea9bde
LypkaB <lpk.bdn@gmail.com>
2020-06-23 20:14:30 +0300

    Add functionality «Add» & «Delete» posts

--- app/src/components/app/app.js
+++ app/src/components/app/app.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import AppHeader from '../app-header';
 import SearchPanel from '../search-panel';
 import PostStatusFilter from '../post-status-filter';
@@ -6,24 +6,60 @@ import PostList from '../post-list';
 import PostAddForm from '../post-add-form';
 import './app.scss';
 
-const App = () => {
-    const data = [
-        {id: 'qwqw', label: 'Going to learn «React»', important: true},
-        {id: 'werr', label: 'That is so good', important: false},
-        {id: 'fdfdf', label: 'I need a break...', important: false}
-    ];
+export default class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [
+                {id: 1, label: 'Going to learn «React»', important: true},
+                {id: 2, label: 'That is so good', important: false},
+                {id: 3, label: 'I need a break...', important: false}
+            ]
+        };
+        this.deleteItem = this.deleteItem.bind(this);
+        this.addItem = this.addItem.bind(this);
 
-    return (
-        <div className="app">
-            <AppHeader/>
-            <div className="search-panel d-flex">
-                <SearchPanel/>
-                <PostStatusFilter/>
+        this.maxId = 4;
+    }
+
+    deleteItem(id) {
+        this.setState(({data}) => {
+            const index = data.findIndex(elem => elem.id === id),
+                  newArr = [...data.slice(0, index), ...data.slice(index + 1)];
+
+            return {
+                data: newArr
+            }
+        });
+    }
+
+    addItem(body) {
+        const newItem = {
+            label: body,
+            important: false,
+            id: this.maxId++
+        };
+
+        this.setState(({data}) => {
+            const newArr = [...data, newItem];
+
+            return {
+                data: newArr
+            }
+        })
+    }
+
+    render() {
+        return (
+            <div className="app">
+                <AppHeader/>
+                <div className="search-panel d-flex">
+                    <SearchPanel/>
+                    <PostStatusFilter/>
+                </div>
+                <PostList posts={this.state.data} onDelete={this.deleteItem} />
+                <PostAddForm onAdd={this.addItem} />
             </div>
-            <PostList posts={data} />
-            <PostAddForm/>
-        </div>
-    )
+        )
+    }
 };
-
-export default App;
--- app/src/components/post-add-form/post-add-form.js
+++ app/src/components/post-add-form/post-add-form.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './post-add-form.scss';
 
-const PostAddForm = () => {
+const PostAddForm = ({onAdd}) => {
     return (
-        <form className="bottom-panel d-flex">
+        <div className="bottom-panel d-flex">
             <input type="text" placeholder="What are you thinking now?" className="form-control new-post-label" />
-            <button type="submit" className="btn btn-outline-secondary">Add</button>
-        </form>
+            <button type="submit" className="btn btn-outline-secondary" onClick={() => onAdd('Hello!')}>Add</button>
+        </div>
     )
 };
 
