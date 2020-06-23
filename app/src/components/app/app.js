3aa331dc89d1609dfecc8e8c1ee39714f0ab830c
LypkaB <lpk.bdn@gmail.com>
2020-06-23 20:59:38 +0300

    Add search by entered word

--- app/src/components/app/app.js
+++ app/src/components/app/app.js
@@ -14,12 +14,14 @@ export default class App extends Component {
                 {id: 1, label: 'Going to learn «React»', important: false, like: false},
                 {id: 2, label: 'That is so good', important: false, like: false},
                 {id: 3, label: 'I need a break...', important: false, like: false}
-            ]
+            ],
+            term: ''
         };
         this.deleteItem = this.deleteItem.bind(this);
         this.addItem = this.addItem.bind(this);
         this.onToggleImportant = this.onToggleImportant.bind(this);
         this.onToggleLiked = this.onToggleLiked.bind(this);
+        this.onUpdateSearch = this.onUpdateSearch.bind(this);
 
         this.maxId = 4;
     }
@@ -77,20 +79,37 @@ export default class App extends Component {
         });
     }
 
+    searchPost(items, term) {
+        if (term.length === 0) {
+            return items
+        }
+
+        return items.filter((item) => {
+            return item.label.indexOf(term) > -1
+        })
+    }
+
+    onUpdateSearch(term) {
+        this.setState({term})
+    }
+
     render() {
-        const {data} = this.state,
+        const {data, term} = this.state,
               liked = data.filter(item => item.like).length,
-              allPosts = data.length;
+              allPosts = data.length,
+              visiblePosts = this.searchPost(data, term);
 
         return (
             <div className="app">
                 <AppHeader liked={liked} allPosts={allPosts} />
+
                 <div className="search-panel d-flex">
-                    <SearchPanel/>
+                    <SearchPanel onUpdateSearch={this.onUpdateSearch} />
                     <PostStatusFilter/>
                 </div>
+
                 <PostList
-                    posts={this.state.data}
+                    posts={visiblePosts}
                     onDelete={this.deleteItem}
                     onToggleImportant={this.onToggleImportant}
                     onToggleLiked={this.onToggleLiked}
