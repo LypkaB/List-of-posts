f0c439d63d9caba3e7824f24622696d96d2a9d0e
LypkaB <lpk.bdn@gmail.com>
2020-06-23 19:48:43 +0300

    Add functionality by pressing buttons «Favorites» & «Like»

--- app/src/components/app/app.js
+++ app/src/components/app/app.js
@@ -7,6 +7,12 @@ import PostAddForm from '../post-add-form';
 import './app.scss';
 
 const App = () => {
+    const data = [
+        {id: 'qwqw', label: 'Going to learn «React»', important: true},
+        {id: 'werr', label: 'That is so good', important: false},
+        {id: 'fdfdf', label: 'I need a break...', important: false}
+    ];
+
     return (
         <div className="app">
             <AppHeader/>
@@ -14,7 +20,7 @@ const App = () => {
                 <SearchPanel/>
                 <PostStatusFilter/>
             </div>
-            <PostList/>
+            <PostList posts={data} />
             <PostAddForm/>
         </div>
     )
