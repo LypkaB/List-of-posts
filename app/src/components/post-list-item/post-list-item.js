b0265db0ab6bfa171c201f532a53f5fa05727aa9
LypkaB <lpk.bdn@gmail.com>
2020-06-23 20:46:10 +0300

    Change styles for buttons «Favorites» & «Like»

--- app/src/components/post-list-item/post-list-item.js
+++ app/src/components/post-list-item/post-list-item.js
@@ -14,17 +14,18 @@ const PostListItem = ({label, onDelete, important, like, onToggleImportant, onTo
 
     return (
         <div className={classNames}>
-            <span className="app-list-item-label" onClick={onToggleLiked}>
-                {label}
-            </span>
+            <span className="app-list-item-label">{label}</span>
+
             <div className="d-flex justify-content-center align-items-center">
                 <button type="button" className="btn-star btn-sm" onClick={onToggleImportant}>
                     <i className="fa fa-star" aria-hidden="true"/>
                 </button>
+                <button type="button" className="btn-star btn-sm" onClick={onToggleLiked}>
+                    <i className="fa fa-heart" aria-hidden="true"/>
+                </button>
                 <button type="button" className="btn-trash btn-sm" onClick={onDelete}>
                     <i className="fa fa-trash-o" aria-hidden="true"/>
                 </button>
-                <i className="fa fa-heart" aria-hidden="true"/>
             </div>
         </div>
     )
